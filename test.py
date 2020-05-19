7b3805b7f7aae72aea560a6fa995a092c8dae3b4
Ataur rahaman <uttardinajpur2015@gmail.com>
2020-05-19 09:35:02 +0530

    this change only for comment

--- test.py
+++ test.py
@@ -8,3 +8,7 @@ def print_raw():
     print("Clear_text")
 decorated_function=my_decorator(print_raw)
 decorated_function()
+
+#The brackets tell python that you are calling the function, 
+#so when you put them there, it calls the function and assigns decorated_function 
+#the value returned by my_decorator(print_raw) (which in this case is None).
