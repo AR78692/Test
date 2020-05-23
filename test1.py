4003cb50712c0b8c3ca0352560ab101bfcb201f7
Ataur rahaman <uttardinajpur2015@gmail.com>
2020-05-23 11:31:37 +0530

    this is the chang for re modouls test

--- test1.py
+++ test1.py
@@ -1,16 +1,16 @@
-print('Programming-------------(3)')
+print('Programming-------------(5)')
 import re
-str='HellO World 4556 welcome'
-pattern=r'[o]+'
-result=re.search(pattern,str,re.I)
+number='123 hi this is a no 879'
+pattern=r'[0-9]+'
+result=re.search(pattern,number)
 if result:
     print('Search():',result.group())
 else:
-    print('Not Match!():')
-result=re.match(pattern,str,re.I)
+    print('Not Match!')
+result=re.match(pattern,number)
 if result:
     print('Match():',result.group())
 else:
-    print('Not Match!():')
-result=re.findall(pattern,str,re.I)
+    print('Not Match')
+result=re.findall(pattern,number)
 print('Findall():',result)
