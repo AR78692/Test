ebeeaede390de935c3c072ed1d5dae5606b103af
Ataur rahaman <uttardinajpur2015@gmail.com>
2020-05-23 10:15:01 +0530

    this is the change for Function and Re modulus

--- test.py
+++ test.py
@@ -1,28 +1,13 @@
-def Add():
-    print('Enter your Frist number:')
-    a=int(input())
-    print('Enter your Second number:')
-    b=int(input())
-    c=a+b
-    print('Your total sum is:',c)
-def Sub():
-    print('Enter your Frist number:')
-    a=int(input())
-    print('Enter your Second number:')
-    b=int(input())
-    c=a-b
-    print('Your total Subtraction is:',c)
-def Mul():
-    print('Enter your Frist number:')
-    a=int(input())
-    print('Enter your Second number:')
-    b=int(input())
-    c=a*b
-    print('Your total Multipliction is:',c)
-def Div():
-    print('Enter your Frist number:')
-    a=int(input())
-    print('Enter your Second number:')
-    b=int(input())
-    c=a/b
-    print('Your total Division is:',c)
+class Ataur:
+    def __init__(self):
+        self.Name='Ataur Rahaman'
+        self.C_Year='Last Year'
+        self.Roll=10
+        self.marks=450
+        self.Category='Pass'
+    def talks(self):
+        print('Your name is:',self.Name)
+        print(self.Name,'You are in:',self.C_Year)
+        print('Dear Student Your Roll No:',self.Roll)
+        print('Wow! Your Result is WellDone! You Are:',self.Category)
+results=Ataur()
--- test1.py
+++ test1.py
@@ -1,7 +1,8 @@
-from test import *
-#this function for adding two value and output there result!
-print('Enter your Numbers for Calculate!')
-Add()
-Sub()
-Mul()
-Div()
+import re
+string='Hello 123 Hello'
+result=re.match(r"(\w+)\ (\d+)\ (\w+)",string)
+print('Group():',result.group())
+print('Group(0):',result.group(0))
+print('Group(1):',result.group(1))
+print('Group(2):',result.group(2))
+print('Group(3):',result.group(3))
